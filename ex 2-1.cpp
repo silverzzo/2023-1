a3eec63a31a3aacd359438620653c993764f851e
silverzzo <124555750+silverzzo@users.noreply.github.com>
2023-03-05 23:18:40 +0900

    20230305
    2장 예제 1번

---
+++ ex 2-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>   //input, output --> io 입출력 관련 cout, cin
+
+int main() {
+	std::cout << "hello\n"; //화면에 hello출력하고 다음줄로 넘어감
+	std::cout << "hello" << std::endl; //위 출력과 동일한 기능을 한다.
+	std::cout << "첫 번째 맛보기 입니다."; //화면에 첫 번째 맛보기 입니다. 출력
+	return 0; //main() 함수가 종료되면 프로그램 종료됨
+}
